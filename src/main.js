b521bbba548539f9213d90950a2a294dd6e19e97
Ale <andreastephanea@gmail.com>
2019-02-24 04:26:51 -0600

    Rompiendo el codigo para nueva version

--- src/main.js
+++ src/main.js
@@ -1,75 +1,96 @@
-const btngetName = document.getElementById('next');
-const trainerName = document.getElementById('nombre');
-const bienvenido = document.getElementById('nombre-bienvenido');
-const sectionName = document.getElementById('pantalla1');
 
-const pantalla2 = document.getElementById('pantalla2');
-const showPokemons = document.getElementById('showPokemons');
-const root = document.getElementById('root');
-const cajitas = document.getElementsByClassName('cajitas')
-const dataPokemon = window.POKEMON.pokemon;
-const forWeight = document.getElementById("forWeight");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const btngetName = document.getElementById('next');
+//const trainerName = document.getElementById('nombre');
+//const bienvenido = document.getElementById('nombre-bienvenido');
+//const sectionName = document.getElementById('pantalla1');
+//const pantalla3 = document.getElementById('pantalla3')
+//const pantalla2 = document.getElementById('pantalla2');
+//const showPokemons = document.getElementById('showPokemons');
+//const root = document.getElementById('root');
+//const cajitas = document.getElementsByClassName('cajitas')
+//const dataPokemon = window.POKEMON.pokemon;
+//const forWeight = document.getElementById("forWeight");
 //const Water = document.getElementById('Water');
 //const Flying = document.getElementById('Flying')
 
-btngetName.addEventListener("click", () => {
-
-  const name = trainerName.value;
-  bienvenido.innerHTML = '¡Hola entrenador ' + name + '. Bienvenido!';
-  sectionName.style.display = "none";
-  pantalla2.style.display = "block"
 
 
+//btngetName.addEventListener("click", () => {
 
-
-
-  })
+  //const name = trainerName.value;
+  //bienvenido.innerHTML = `<div class= "nombre">¡Hola entrenador ${name} Bienvenido! </div>`;
+  //sectionName.style.display = "none";
+  //pantalla2.style.display = "block"
 
 
 
 
 
-const printData = (data) => {
-  root.innerHTML = '';
-
-  data.forEach(pokemon => {
-    let namePokemon = `<figure class="tarjeta"> <h1> ${pokemon.num} </h1> <h2> ${pokemon.name} </h2>    <div> <img = class "imageBox" src ="${pokemon.img}"> </div>
-    <p> TYPE: ${pokemon.type} </p>
-    <p>HEIGHT:${pokemon.height}</p>
-    <p>WEIGHT:${pokemon.weight}</p>
-    <p>CANDY:${pokemon.candy}</p>
-    <p>CANDY COUNT:${pokemon.candy_count}</p>
-    <p>EGG: ${pokemon.egg}</p>
-    <p>SPAWN CHANCE: ${pokemon.spawn_chance}</p>
-    <p>AVERANGE SPAWNS: ${pokemon.avg_spawns}</p>
-    <p>SPAWN TIME:${pokemon.spawn_time}</p>
-    <p>MULTIPLIERS:${pokemon.multipliers}</p>
-    <p>WEAKNESSES:${pokemon.weaknesses}</p> </figure>
-    `
-
-    root.insertAdjacentHTML("beforeend", namePokemon);
-
-
-  })
-}
+  //})
 
 
 
-for (let i = 0; i < cajitas.length; i++) {
-  cajitas[i].addEventListener('click', () => {
-    let elementPokemon = cajitas[i].id;
-    let pokemonType = window.pokemon.filterPokemons(dataPokemon, elementPokemon);
-    window.pokemon.ordenarPorNombre(pokemonType);
-    printData(pokemonType);
-
-  });
 
 
-}
+//const printData = (data) => {
+  //root.innerHTML = '';
 
-forWeight.addEventListener("click",() => {
-  const pokemonTotalPeso =  window.pokemon.totalWeightPokemon(dataPokemon);
-  root.innerHTML= pokemonTotalPeso;
+  //data.forEach(pokemon => {
+    //let namePokemon = `<figure class="tarjeta"> <h1> ${pokemon.num} </h1> <h2> ${pokemon.name} </h2>    <div> <img = class "imageBox" src ="${pokemon.img}"> </div>
+    //<p> TYPE: ${pokemon.type} </p>
+    //<p>HEIGHT:${pokemon.height}</p>
+    //<p>WEIGHT:${pokemon.weight}</p>
+   // <p>CANDY:${pokemon.candy}</p>
+   // <p>CANDY COUNT:${pokemon.candy_count}</p>
+   // <p>EGG: ${pokemon.egg}</p>
+    //<p>SPAWN CHANCE: ${pokemon.spawn_chance}</p>
+    //<p>AVERANGE SPAWNS: ${pokemon.avg_spawns}</p>
+    //<p>SPAWN TIME:${pokemon.spawn_time}</p>
+    //<p>MULTIPLIERS:${pokemon.multipliers}</p>
+    //<p>WEAKNESSES:${pokemon.weaknesses}</p> </figure>
+    //`
+
+    //root.insertAdjacentHTML("beforeend", namePokemon);
+
+
+  //})
+//}
+
+
+
+//for (let i = 0; i < cajitas.length; i++) {
+  //cajitas[i].addEventListener('click', () => {
+    //let elementPokemon = cajitas[i].id;
+    //let pokemonType = window.pokemon.filterPokemons(dataPokemon, elementPokemon);
+    //window.pokemon.ordenarPorNombre(pokemonType);
+    //printData(pokemonType);
+
+  //});
+
+
+//}
+
+//forWeight.addEventListener("click",() => {
+  //const pokemonTotalPeso =  window.pokemon.totalWeightPokemon(dataPokemon);
+  //root.innerHTML= pokemonTotalPeso;
 //  root.innerHTML = '';
   //console.log(pokemonTotalPeso);
  //const parrafo= `<p>Peso total de los Pokemon:${pokemonTotalPeso}</p>`
@@ -78,10 +99,10 @@ forWeight.addEventListener("click",() => {
 
 
 
-});
+//});
 
 
-showPokemons.addEventListener("click",() => {
-   printData(dataPokemon)
+//showPokemons.addEventListener("click",() => {
+  // printData(dataPokemon)
 
-})
+//})
